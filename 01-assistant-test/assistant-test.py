b60120f97789c7b025fddb4533e0a8cf13d69cc7
Italo Lui <italorlui@gmail.com>
2025-09-20 19:50:05 -0300

    First local assistant test (OpenAI gpt-oss-20b)

--- 01-assistant-test/assistant-test.py
+++ 01-assistant-test/assistant-test.py
@@ -1,7 +1,45 @@
 from autogen_agentchat.agents import AssistantAgent
 from autogen_ext.models.openai import OpenAIChatCompletionClient
+from autogen_core import CancellationToken
+from autogen_core.models import ModelFamily
+from autogen_agentchat.messages import TextMessage
 from dotenv import load_dotenv
 import os
+import asyncio
 
-load_dotenv()
-api_key = os.getenv('OPENAI_API_KEY')
+async def main() -> None:
+    load_dotenv()
+    lm_model = os.getenv('LM_MODEL')
+    base_url = os.getenv('BASE_URL')
+    api_key  = os.getenv('API_KEY')
+
+    custom_model_client = OpenAIChatCompletionClient(
+        model = lm_model, #the name of your running model
+        base_url = base_url, #the local address of the api
+        api_key = api_key, # just a placeholder
+        model_info = {
+            "vision": False,
+            "function_calling": False,
+            "json_output": False,
+            "family": ModelFamily.UNKNOWN,
+            "structured_output": False,
+        },
+        seed = 42,  # seed for caching and reproducibility
+        temperature = 0,  # temperature for sampling
+        timeout = 400, # timeout,
+    )
+
+    assistant = AssistantAgent(
+        name = "assistant",
+        model_client = custom_model_client,
+        description = "A basic first Agent",
+    )
+
+    cancellation_token = CancellationToken()
+
+    response = await assistant.on_messages([TextMessage(content="Hello!", source="user")], cancellation_token)
+    print(response)
+
+    custom_model_client.close()
+
+asyncio.run(main())
