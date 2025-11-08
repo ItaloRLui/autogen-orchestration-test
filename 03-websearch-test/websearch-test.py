818ffdd99b5dc0d27751bab5e274324608f59719
Italo Lui <italorlui@gmail.com>
2025-11-07 22:43:14 -0300

    Websearch update

--- 03-websearch-test/websearch-test.py
+++ 03-websearch-test/websearch-test.py
@@ -4,6 +4,7 @@ from autogen_ext.models.ollama import OllamaChatCompletionClient
 from autogen_core.models import ModelFamily
 from autogen_agentchat.teams import RoundRobinGroupChat
 from autogen_agentchat.ui import Console
+from autogen_agentchat.base import TaskResult
 from dotenv import load_dotenv
 import os
 import asyncio
@@ -28,22 +29,24 @@ async def main() -> None:
     web_surfer_agent = MultimodalWebSurfer(
         name = "MultimodalWebSurfer",
         model_client = custom_model_client,
-        description = "A multimodal agent that can interact with a Chromium-based browser to complete tasks."
+        description = "A multimodal agent that can interact with a Chromium-based browser to complete tasks.",
     )
 
     boss = AssistantAgent(
         name = "organizer",
         model_client = custom_model_client,
-        system_message = "Tell the other agents the next steps necessary to complete the given task. The other agents are able" \
-        "to use the internet and search for information.",
+        system_message = "You are an organizer. You are in a chat with an agent that can search the Internet." \
+        "You must tell the other agent what is the next step that they must take to progress the task given to you." \
+        "The web searching agent can't give any feedback to you, only showing what is on the page." \
+        "Always give only one step at a time." \
+        "The output must be as requested. You cannot search the Internet, and must not attempt to do web searches." \
+        "You may only direct the other agent to do web searches.",
         description = "An agent that coordinates the other agents efforts in a group chat to complete tasks.",
         model_client_stream = True,
     )
 
     agent_team = RoundRobinGroupChat(
         participants = [boss, web_surfer_agent],
-        name = "Round Robin Web Surfer Group Chat",
-        description = "Group chat that the web surfer agent uses",
         # max_turns = 3,
     )
     # Participantes do chat postam mensagens para todos os outros participantes
